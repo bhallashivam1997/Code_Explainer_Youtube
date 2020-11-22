94bf9f948ed3c7c289b45f7913baded6a7d2bb41
bhallashivam1997 <72436501+bhallashivam1997@users.noreply.github.com>
2020-11-23 01:47:48 +0530

    Create Super_Ryuma.cpp

---
+++ atcoder/Super_Ryuma.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+        #include <math.h>
+    
+        using namespace std;
+        typedef long long ll;
+        typedef unsigned long long ull;
+        # define M_PI  3.14159265358979323846
+    
+    
+        const int M=1e9+7;
+        long long mod(long long x){
+            return ((x%M + M)%M);
+        }
+        long long add(long long a, long long b){
+            return mod(mod(a)+mod(b));
+        }
+        long long mul(long long a, long long b){
+            return mod(mod(a)*mod(b));
+        }
+    
+        ll modPow(ll a, ll b){
+            if(b==0)
+                return 1LL;
+            if(b==1)
+                return a%M;
+            ll res=1;
+            while(b){
+                if(b%2==1)
+                    res=mul(res,a);
+                a=mul(a,a);
+                b=b/2;
+            }
+            return res;
+        }
+
+        bool check(ll r1, ll c1, ll r2 , ll c2){
+            return ((r1+c1)==(r2+c2) || (r1-c1)==(r2-c2)|| (abs(r1-r2)+abs(c1-c2)<=3));
+        }
+        void solve(){
+            ll r1,c1,r2,c2;
+            cin>>r1>>c1>>r2>>c2;
+            if(r1>r2){
+                swap(r1,r2);
+                swap(c1,c2);
+            }
+            if(r1==r2 && c1==c2){
+                cout<<0;
+                return;
+            }
+            if(check(r1,c1,r2,c2)){
+                cout<<1;
+                return;
+            }
+            if((r1+c1)%2 == (r2+c2)%2){
+                cout<<2;
+                return;
+            }
+            for(ll i=-2;i<=2;i++){
+                for(ll j=-2;j<=2;j++){
+                    ll p=r2+i;
+                    ll q=c2+j;
+                    if(check(r1,c1,p,q)){
+                        cout<<2;
+                        return;
+                    } 
+                }
+            }
+            if(check(r1,c1,r2+3,c2) || check(r1,c1,r2-3,c2) || check(r1,c1,r2,c2+3) || check(r1,c1,r2,c2-3)){
+                cout<<2;
+                return;
+            }
+            cout<<3;
+        }
+        int main(){
+            ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            cout<<fixed;
+            cout<<setprecision(10);
+    //        freopen("timber_input.txt", "r", stdin);
+    //        freopen("timber_output.txt", "w", stdout);
+            int t=1;
+            // cin>>t;
+            for(int i=1;i<=t;i++){
+            //    cout<<"Case #"<<i<<": ";
+                solve();
+            }
+            return 0;
+        }
