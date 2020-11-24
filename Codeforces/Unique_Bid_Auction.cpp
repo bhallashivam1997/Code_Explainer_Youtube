25db992e622681170cf95bb4b2e40d2cae93a363
bhallashivam1997 <72436501+bhallashivam1997@users.noreply.github.com>
2020-11-25 03:40:08 +0530

    Create Unique_Bid_Auction.cpp

---
+++ Codeforces/Unique_Bid_Auction.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+        #include <math.h>
+    
+        using namespace std;
+        typedef long long ll;
+        typedef unsigned long long ull;
+        # define M_PI  3.14159265358979323846
+    
+    
+        const int M=1e9+7;
+        long long mod(long long x){
+            return ((x%M + M)%M);
+        }
+        long long add(long long a, long long b){
+            return mod(mod(a)+mod(b));
+        }
+        long long mul(long long a, long long b){
+            return mod(mod(a)*mod(b));
+        }
+    
+        ll modPow(ll a, ll b){
+            if(b==0)
+                return 1LL;
+            if(b==1)
+                return a%M;
+            ll res=1;
+            while(b){
+                if(b%2==1)
+                    res=mul(res,a);
+                a=mul(a,a);
+                b=b/2;
+            }
+            return res;
+        }
+ 
+        void solve(){
+            int n;
+            cin>>n;
+            map<int,int>mp;
+            vector<int>a(n);
+            for(int i=0;i<n;i++){
+                int x;
+                cin>>x;
+                a[i]=x;
+                mp[x]++;
+            }
+            for(auto i : mp){
+                if(i.second==1){
+                    for(int j=0;j<n;j++){
+                        if(a[j]==i.first){
+                            cout<<j+1<<"\n";
+                            return;
+                        }
+                    }
+                }
+            }
+            cout<<-1<<"\n";
+        }
+        int main(){
+            ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            cout<<fixed;
+            cout<<setprecision(10);
+    //        freopen("timber_input.txt", "r", stdin);
+    //        freopen("timber_output.txt", "w", stdout);
+            int t=1;
+            cin>>t;
+            for(int i=1;i<=t;i++){
+            //    cout<<"Case #"<<i<<": ";
+                solve();
+            }
+            return 0;
+        }
