c33a550e36b8708e02d1c4d44a85d3764d8e2d18
bhallashivam1997 <72436501+bhallashivam1997@users.noreply.github.com>
2020-11-29 03:14:09 +0530

    Create hands.cpp

---
+++ atcoder/hands.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+        #include <math.h>
+    
+        using namespace std;
+        typedef long long ll;
+        typedef unsigned long long ull;
+        # define M_PI  3.14159265358979323846
+    
+    
+        const int M=1e9+7;
+        long long mod(long long x){
+            return ((x%M + M)%M);
+        }
+        long long add(long long a, long long b){
+            return mod(mod(a)+mod(b));
+        }
+        long long mul(long long a, long long b){
+            return mod(mod(a)*mod(b));
+        }
+    
+        ll modPow(ll a, ll b){
+            if(b==0)
+                return 1LL;
+            if(b==1)
+                return a%M;
+            ll res=1;
+            while(b){
+                if(b%2==1)
+                    res=mul(res,a);
+                a=mul(a,a);
+                b=b/2;
+            }
+            return res;
+        }
+ 
+        void solve(){
+            int a,b,x,y;
+            cin>>a>>b>>x>>y;
+            if(a==b){
+                cout<<x;
+            }
+            else if(a<b){
+                int diff=b-a;
+                int ans=min(diff*y + x , diff*2*x + x);
+                cout<<ans;
+            }
+            else{
+                int diff=a-b;
+                int ans=min({diff*y + x , (diff-1)*2*x + x,(diff-1)*y + x});
+                cout<<ans;
+            }
+        }
+        int main(){
+            ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            cout<<fixed;
+            cout<<setprecision(10);
+    //        freopen("timber_input.txt", "r", stdin);
+    //        freopen("timber_output.txt", "w", stdout);
+            int t=1;
+            // cin>>t;
+            for(int i=1;i<=t;i++){
+            //    cout<<"Case #"<<i<<": ";
+                solve();
+            }
+            return 0;
+        }
