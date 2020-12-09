3150c2ab0e58a5462a9858125448c3474cf48f06
bhallashivam1997 <72436501+bhallashivam1997@users.noreply.github.com>
2020-12-10 03:21:53 +0530

    Create nCr.cpp

---
+++ Codeforces/nCr.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+        #include <math.h>
+    
+        using namespace std;
+        typedef long long ll;
+        typedef unsigned long long ull;
+        # define M_PI  3.14159265358979323846
+    
+    
+        // const int M=1e9+7;
+        const int M=998244353;
+        long long mod(long long x){
+            return ((x%M + M)%M);
+        }
+        long long add(long long a, long long b){
+            return mod(mod(a)+mod(b));
+        }
+        long long mul(long long a, long long b){
+            return mod(mod(a)*mod(b));
+        }
+    
+        ll modPow(ll a, ll b){
+            if(b==0)
+                return 1LL;
+            if(b==1)
+                return a%M;
+            ll res=1;
+            while(b){
+                if(b%2==1)
+                    res=mul(res,a);
+                a=mul(a,a);
+                b=b/2;
+            }
+            return res;
+        }
+
+        const int N=2e5+2;
+        int fact[N];
+
+        void precalc(){
+            fact[0]=1;
+            for(int i=1;i<N;i++){
+                fact[i]=mul(fact[i-1],i);
+            }
+        }
+
+        ll inv(ll x){
+            return modPow(x,M-2);
+        }
+
+        ll divide(ll a, ll b){
+            return mul(a,inv(b));
+        }
+
+        ll nCr(ll n, ll r){
+            return divide(fact[n],mul(fact[r],fact[n-r]));
+        }
+
+        void solve(){
+            
+        }
+        int main(){
+            ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            cout<<fixed;
+            cout<<setprecision(10);
+    //        freopen("timber_input.txt", "r", stdin);
+    //        freopen("timber_output.txt", "w", stdout);
+            int t=1;
+            // cin>>t;
+            for(int i=1;i<=t;i++){
+            //    cout<<"Case #"<<i<<": ";
+                solve();
+            }
+            return 0;
+        }
